c6071dd5bdf80b4d597d1e1acf42a5da5f6ae6c4
Time_Traveller <terepapakamaal@proton.me>
2025-06-10 23:31:41 +0530

    files updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// filepath: e:\download\material-ui-7.1.1\material-ui-7.1.1\docs\data\material\getting-started\templates\meow\src\App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import AppTheme from './shared-theme/AppTheme';
+import Blog from './Blog';
+import SignIn from './SignIn';
+import SignUp from './SignUp'; // Import the SignUp component
+import AppAppBar from './components/AppAppBar'; // Assuming you want the app bar on all pages
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppTheme>
+        {/* AppAppBar can be placed here if you want it on every page */}
+        {/* For this example, Blog will include its own AppAppBar */}
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/blog" />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          {/* Add other routes here */}
+        </Routes>
+      </AppTheme>
+    </Router>
   );
 }
 
